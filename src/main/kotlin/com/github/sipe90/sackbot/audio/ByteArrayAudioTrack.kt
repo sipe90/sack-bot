bb6f68c4671b219c6c13cba835f7ef37075ddb3f
Pasi Härme <pasi.harme@gmail.com>
2020-03-18 19:43:25 +0200

    Use NonSeekableInputStream

--- src/main/kotlin/com/github/sipe90/sackbot/audio/ByteArrayAudioTrack.kt
+++ src/main/kotlin/com/github/sipe90/sackbot/audio/ByteArrayAudioTrack.kt
@@ -2,11 +2,13 @@ package com.github.sipe90.sackbot.audio
 
 import com.sedmelluq.discord.lavaplayer.container.MediaContainerDescriptor
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager
+import com.sedmelluq.discord.lavaplayer.tools.io.NonSeekableInputStream
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo
 import com.sedmelluq.discord.lavaplayer.track.DelegatedAudioTrack
 import com.sedmelluq.discord.lavaplayer.track.InternalAudioTrack
 import com.sedmelluq.discord.lavaplayer.track.playback.LocalAudioTrackExecutor
+import java.io.ByteArrayInputStream
 
 class ByteArrayAudioTrack(
     private val audio: ByteArray,
@@ -16,7 +18,7 @@ class ByteArrayAudioTrack(
 ) : DelegatedAudioTrack(trackInfo) {
 
     override fun process(localExecutor: LocalAudioTrackExecutor?) {
-        SeekableBufferedInputStream(audio).use { inputStream ->
+        NonSeekableInputStream(ByteArrayInputStream(audio)).use { inputStream ->
             processDelegate(
                 containerTrackFactory.createTrack(
                     trackInfo,
