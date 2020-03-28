e2a6b8e2b76015081aaf5d77490994fda92744d8
Pasi Härme <pasi.harme@gmail.com>
2020-03-28 20:44:11 +0200

    Group sound by first letter of name

--- src/main/webapp/components/Soundboard.tsx
+++ src/main/webapp/components/Soundboard.tsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useCallback } from 'react'
 import styled from 'styled-components'
+import * as R from 'ramda'
+import { Button, Divider } from 'antd'
 
-import { Button } from 'antd';
-import { useDispatch, useSelector } from '@/util';
-import { fetchSounds, playSound, playRandomSound } from '@/actions/sounds';
+import { useDispatch, useSelector } from '@/util'
+import { fetchSounds, playSound, playRandomSound } from '@/actions/sounds'
+import { IAudioFile } from '@/types'
 
-const SoundboardRoot = styled.div`
+const SoundGrid = styled.div`
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: flex-start;
 `
 
 const Sound = styled.div`
     margin: 5px;
     flex: 0 0 0;
 `
+
+const groupByFirstLetter = R.groupBy<IAudioFile>(R.pipe(
+    R.pathOr('', ['name', 0]),
+    R.toUpper
+))
 
 const Soundboard: React.FC = () => {
 
@@ -36,19 +43,11 @@ const Soundboard: React.FC = () => {
         selectedGuild && dispatch(fetchSounds(selectedGuild))
     }, [selectedGuild])
 
+    const groupedSounds = groupByFirstLetter(sounds)
+    const letters = R.keys(groupedSounds).sort()
+
     return (
-        <SoundboardRoot>
-            {sounds.map((sound) => 
-                <Sound key={sound.name}>
-                    <Button
-                        block
-                        style={{ width: 120 }}
-                        disabled={playingSound}
-                        onClick={() => selectedGuild && dispatch(playSound(selectedGuild, sound.name))}>
-                            <div style={{ overflow: "hidden", textOverflow: "ellipsis"}}>{sound.name}</div>
-                    </Button>
-                </Sound>
-            )}
+        <>
             <Sound>
                 <Button
                     block
@@ -59,7 +58,24 @@ const Soundboard: React.FC = () => {
                         <div style={{ overflow: "hidden", textOverflow: "ellipsis"}}>Random</div>
                 </Button>
             </Sound>
-        </SoundboardRoot>
+            {letters.map((letter) => 
+                <div key={letter}>
+                    <Divider>{letter}</Divider>
+                    <SoundGrid>
+                        {groupedSounds[letter].map((sound) => 
+                            <Sound key={sound.name}>
+                                <Button
+                                    block
+                                    style={{ width: 120 }}
+                                    disabled={playingSound}
+                                    onClick={() => selectedGuild && dispatch(playSound(selectedGuild, sound.name))}>
+                                        <div style={{ overflow: "hidden", textOverflow: "ellipsis"}}>{sound.name}</div>
+                                </Button>
+                            </Sound>)}
+                    </SoundGrid>
+                </div>
+            )}
+        </>
     )
 }
 
