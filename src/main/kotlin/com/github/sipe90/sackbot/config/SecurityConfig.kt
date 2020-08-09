b52983ddd3bdc18b51dfbad19208704615c2ccb1
Pasi Härme <pasi.harme@gmail.com>
2020-08-09 16:16:23 +0300

    Improved login page

--- src/main/kotlin/com/github/sipe90/sackbot/config/SecurityConfig.kt
+++ src/main/kotlin/com/github/sipe90/sackbot/config/SecurityConfig.kt
@@ -9,17 +9,26 @@ import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import org.springframework.security.oauth2.core.OAuth2Error
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes
 import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.security.web.server.DefaultServerRedirectStrategy
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.ServerRedirectStrategy
+import org.springframework.security.web.server.WebFilterExchange
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
+import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
+import java.net.URI
 
 @EnableWebFluxSecurity
 class SecurityConfig {
@@ -36,6 +45,7 @@ class SecurityConfig {
                 .oauth2Client()
                 .and()
                 .oauth2Login()
+                .authenticationFailureHandler(AuthenticationFailureHandler())
                 .and()
                 .logout()
                 .and()
@@ -65,6 +75,10 @@ class SecurityConfig {
                 val userId = user.attributes[DiscordUser.Attributes.ID] as String
                 val mutualGuilds = jdaService.getMutualGuilds(userId)
 
+                if (mutualGuilds.isEmpty() || true) {
+                    throw OAuth2AuthenticationException(OAuth2Error(OAuth2ErrorCodes.ACCESS_DENIED))
+                }
+
                 DiscordUser(
                         buildAuthorities(userId, mutualGuilds),
                         user.attributes.filterKeys(attributes::contains)
@@ -78,6 +92,24 @@ class SecurityConfig {
             val member = it.getMemberById(userId) ?: throw RuntimeException("Member not found")
             val roles = member.roles.map(Role::getName).toSet()
             DiscordAuthority(it.id, roles, member.isOwner)
+        }
+    }
+
+    class AuthenticationFailureHandler() : ServerAuthenticationFailureHandler {
+
+        private var redirectStrategy: ServerRedirectStrategy = DefaultServerRedirectStrategy()
+        private var fallbackLocation = URI.create("/login?error")
+
+        override fun onAuthenticationFailure(
+                webFilterExchange: WebFilterExchange, exception: AuthenticationException): Mono<Void> {
+
+            var location = fallbackLocation
+
+            if (exception is OAuth2AuthenticationException) {
+                location = URI.create("/login?error=${exception.error.errorCode}")
+            }
+
+            return redirectStrategy.sendRedirect(webFilterExchange.exchange, location)
         }
     }
 }
