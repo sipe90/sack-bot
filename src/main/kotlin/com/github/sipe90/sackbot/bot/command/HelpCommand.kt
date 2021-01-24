fff7e5ea04669e2d0ad31c00974c86b1113cd0a1
Pasi Härme <pasi.harme@gmail.com>
2021-01-24 12:17:45 +0200

    Remove old chat commands from help

--- src/main/kotlin/com/github/sipe90/sackbot/bot/command/HelpCommand.kt
+++ src/main/kotlin/com/github/sipe90/sackbot/bot/command/HelpCommand.kt
@@ -22,32 +22,31 @@ class HelpCommand(private val config: BotConfig) : BotCommand() {
         }
 
         response.append("The following chat commands are available:\n")
-                .append("```")
-                .append(helpLine("help", "Prints this text"))
-                .append(helpLine("list", "Lists all playable sound names"))
-                .append(helpLine("play <sound_name> [volume]", "Plays a sound with the given name"))
-                .append(helpLine("url <url> [volume]", "Play audio from a web source"))
-                .append(helpLine("rnd", "Play a random sound"))
-                .append(helpLine("entry <sound_name>", "Play a sound when you enter a voice channel"))
-                .append(helpLine("exit <sound_name>", "Play a sound when you exit a voice channel"))
-                .append(helpLine("rnd", "Play a random sound"))
-                .append(helpLine("say <voice> <text>", "Play voice lines"))
-                .append(helpLine("tts <text>", "Text to speech"))
-                .append(
-                        helpLine(
-                                "volume <1-100>",
-                                "Set default sound playback volume or just ${config.chat.commandPrefix}volume to get the current volume"
-                        )
+            .append("```")
+            .append(helpLine("help", "Prints this text"))
+            .append(helpLine("list", "Lists all playable sound names"))
+            .append(helpLine("play <sound_name> [volume]", "Plays a sound with the given name"))
+            .append(helpLine("url <url> [volume]", "Play audio from a web source"))
+            .append(helpLine("rnd", "Play a random sound"))
+            .append(helpLine("entry <sound_name>", "Play a sound when you enter a voice channel"))
+            .append(helpLine("exit <sound_name>", "Play a sound when you exit a voice channel"))
+            .append(helpLine("rnd", "Play a random sound"))
+            .append(
+                helpLine(
+                    "volume <1-100>",
+                    "Set default sound playback volume or just ${config.chat.commandPrefix}volume to get the current volume"
                 )
-                .append("```\n")
-                .append("You can also send me new audio files via a private message. Supported file types are `.wav (PCM 16-bit)`, `.mp3` and `.ogg`.").toString()
+            )
+            .append("```\n")
+            .append("You can also send me new audio files via a private message. Supported file types are `.wav (PCM 16-bit)`, `.mp3` and `.ogg`.")
+            .toString()
     }.flux()
 
     private fun helpLine(cmd: String, text: String): StringBuilder {
         return StringBuilder("\n")
-                .append(config.chat.commandPrefix)
-                .append(cmd)
-                .append("                          -- ".substring(cmd.length - 1))
-                .append(text)
+            .append(config.chat.commandPrefix)
+            .append(cmd)
+            .append("                          -- ".substring(cmd.length - 1))
+            .append(text)
     }
 }
